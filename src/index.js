3471960df2fd5d3f8bf2e10a47a2058aa8ac7f20
satopu12 <86809675+satopu12@users.noreply.github.com>
2022-05-14 11:22:24 +0900

    adding the javascript to create the list item for the button click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+const post = document.getElementById("post");
+post.addEventListener("click", function () {
+    let commentsValue = document.getElementById("comments").value;
+
+    let li = document.createElement("li");
+    let text = document.createTextNode(commentsValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+
+});
